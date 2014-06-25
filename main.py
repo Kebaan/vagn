a5fe4847e886913a1b8d33293419ca48ad75dc97
Christian Gram Kalhauge <christian@kalhauge.dk>
2014-06-26 00:57:33 +0200

    Added the basic layout

--- main.py
+++ main.py
@@ -2,4 +2,18 @@
 This is the main application for runnnimg `vagn`.
 """
 
-from interface import MpdClient
+from interface import MPDClient
+
+from kivy.app import App
+from kivy.uix.boxlayout import BoxLayout
+
+class MusicWidget(BoxLayout):
+
+    def add_song(self, name):
+        self.songs.item_strings = []
+
+class VagnApp(App):
+    pass
+
+if __name__ == '__main__':
+    VagnApp().run()
